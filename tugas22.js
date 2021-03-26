43d28a5601aa489b20b2f92852b15d18cd25bf9c
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-03-26 14:20:44 +0700

    Tugas 22 Javascript
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ tugas22.js
@@ -0,0 +1,5 @@
+let kalimat = "Saya ingin belajar bersama";
+let hasilOutput = kalimat.split(" ");
+hasilOutput.forEach(function (item, index) {
+  console.log("Item : "+item+" "+"Index ke "+index);
+})
